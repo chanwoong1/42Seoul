67fed6fefa06a0b1b845c41665d29e9aeb46c434
chanwoong1 <jcw7468@gmail.com>
2023-01-06 18:18:03 +0900

    start: ft_container subject start

--- Circle_4/cpp_module/cpp07/ex02/main.cpp
+++ Circle_4/cpp_module/cpp07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: chanwjeo <chanwjeo@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 14:45:02 by chanwjeo          #+#    #+#             */
-/*   Updated: 2022/12/31 02:40:22 by chanwjeo         ###   ########.fr       */
+/*   Updated: 2023/01/06 10:45:56 by chanwjeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,3 +66,58 @@ int main() {
   }
   return 0;
 }
+
+
+// #include <iostream>
+// #include "Array.hpp"
+
+// #define MAX_VAL 750
+// int main(int, char**)
+// {
+//     Array<int> numbers(MAX_VAL);
+//     int* mirror = new int[MAX_VAL];
+//     srand(time(NULL));
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         const int value = rand();
+//         numbers[i] = value;
+//         mirror[i] = value;
+//     }
+//     //SCOPE
+//     {
+//         Array<int> tmp = numbers;
+//         Array<int> test(tmp);
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         if (mirror[i] != numbers[i])
+//         {
+//             std::cerr << "didn't save the same value!!" << std::endl;
+//             return 1;
+//         }
+//     }
+//     try
+//     {
+//         numbers[-2] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+//     try
+//     {
+//         numbers[MAX_VAL] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         numbers[i] = rand();
+//     }
+//     delete [] mirror;//
+//     return 0;
+// }
--- Circle_4/cpp_module/cpp08/ex00/easyfind.hpp
+++ Circle_4/cpp_module/cpp08/ex00/easyfind.hpp
@@ -6,14 +6,20 @@
 /*   By: chanwjeo <chanwjeo@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 11:27:19 by chanwjeo          #+#    #+#             */
-/*   Updated: 2023/01/03 01:20:43 by chanwjeo         ###   ########.fr       */
+/*   Updated: 2023/01/06 13:22:00 by chanwjeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <algorithm>
+#include <deque>
 #include <iostream>
+#include <list>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
 template <typename T>
 typename T::iterator easyfind(T& container, int value) {
