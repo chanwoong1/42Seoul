6250df49050f58bca709a18b40869aba0210f1a3
chanwoong1 <jcw7468@gmail.com>
2023-09-20 17:47:17 +0900

    07 clear

--- Outer/pythonPiscine/00-Starting/ex07/sos.py
+++ Outer/pythonPiscine/00-Starting/ex07/sos.py
@@ -2,43 +2,43 @@ import sys
 
 
 NEST_MORSE = {
-    " " : "/",
-    "A" : ".-",
-    "B" : "-...",
-    "C" : "-.-.",
-    "D" : "-..",
-    "E" : ".",
-    "F" : "..-.",
-    "G" : "--.",
-    "H" : "....",
-    "I" : "..",
-    "J" : ".---",
-    "K" : "-.-",
-    "L" : ".-..",
-    "M" : "--",
-    "N" : "-.",
-    "O" : "---",
-    "P" : ".--.",
-    "Q" : "--.-",
-    "R" : ".-.",
-    "S" : "...",
-    "T" : "-",
-    "U" : "..-",
-    "V" : "...-",
-    "W" : ".--",
-    "X" : "-..-",
-    "Y" : "-.--",
-    "Z" : "--..",
-    "0" : "-----",
-    "1" : ".----",
-    "2" : "..---",
-    "3" : "...--",
-    "4" : "....-",
-    "5" : ".....",
-    "6" : "-....",
-    "7" : "--...",
-    "8" : "---..",
-    "9" : "----."
+    " ": "/",
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----."
 }
 
 
@@ -64,21 +64,22 @@ def sos(args) -> str:
     Returns:
         str: the string converted to morse
     """
-    return " ".join([NEST_MORSE[c] for c in args])
+    return " ".join(
+        [NEST_MORSE[c.upper()] for c in args if c.isalpha() or c in NEST_MORSE]
+            )
 
 
-
-def main() :
+def main():
     """main function
 
     Returns:
         int: 0 if success, 1 if error
     """
     args = sys.argv[1:]
-    if len(args) != 1 :
+    if len(args) != 1:
         print("AssertionError: the arguments are bad", file=sys.stderr)
         return 1
-    if validate_args(args[0]) == False :
+    if validate_args(args[0]) is False:
         print("AssertionError: the arguments are bad", file=sys.stderr)
         return 1
     print(sos(args[0]))
