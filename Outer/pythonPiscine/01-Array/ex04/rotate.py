25d0911065ab6551ddab6f2842d34e5efe86832f
Chanwoong1 <jcw7468@gmail.com>
2023-10-06 10:01:32 +0900

    하는중

--- Outer/pythonPiscine/01-Array/ex04/rotate.py
+++ Outer/pythonPiscine/01-Array/ex04/rotate.py
@@ -1,33 +1,60 @@
 from load_image import ft_load
-from matplotlib import pyplot as plt
 import numpy as np
+from matplotlib import pyplot as plt
 
 
-# def rotate():
-#     loaded_image = ft_load("animal.jpeg")
-#     if loaded_image is not None:
-#         height, width = loaded_image.shape[:2]
-#         x = width // 2
-#         y = height // 2
-#         cropped = loaded_image[
-#             y - 200 : y + 200,
-#             x - 200 : x + 200,
-#         ]
-#         rotated_image = np.rot90(cropped)
-#         print(rotated_image.shape)
-#         print(rotated_image)
-#         plt.imshow(
-#             rotated_image, cmap="gray" if len(rotated_image.shape) == 2 else None
-#         )
-#         plt.show()
-#     else:
-#         print("Error loading image")
+def plotting(imgarr: np.array):
+    plt.imshow(imgarr[:, :, 0:], cmap="gray")
+    plt.show()
 
 
-def main():
-    """main()"""
-    loaded_image = ft_load("animal.jpeg")
+def crop(image: np.array) -> np.array:
+    height, width = image.shape[:2]
+
+    mid_h = height // 2
+    mid_w = width // 2
+
+    offset_x = 150
+    offset_y = -220
+
+    left = mid_w - 200 + offset_x
+    right = mid_w + 200 + offset_x
+    top = mid_h - 200 + offset_y
+    bottom = mid_h + 200 + offset_y
+
+    crop_img = image[top:bottom, left:right, :3]
+    res = np.mean(crop_img, axis=2, keepdims=True).astype(np.uint8)
+    print(f"The shape of the image is: {res.shape}")
+    print(res)
+    return res
+
+
+def transpose(img_arr: np.array) -> np.array:
+    rows, cols, channels = img_arr.shape
+    tr = np.empty((cols, rows, channels), dtype=img_arr.dtype)
+
+    for i in range(rows):
+        for j in range(cols):
+            tr[j][i] = img_arr[i][j]
+    print(f"New shape after transpose: {tr.shape[0], tr.shape[1]}")
+    print(tr[:, :, 0])
+    return tr
+
+
+def rotate():
+    try:
+        img_file = "animal.jpeg"
+        err_msg = "AssertionError: Unable to rotate"
+        loaded_image = ft_load(img_file)
+        assert loaded_image is not None, err_msg
+
+        crop_img = crop(loaded_image)
+        tr = transpose(crop_img)
+        plotting(tr)
+
+    except AssertionError as msg:
+        print(msg)
 
 
 if __name__ == "__main__":
-    main()
+    rotate()
